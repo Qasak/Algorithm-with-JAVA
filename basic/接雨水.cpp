7954fdb107591c93bf80901c6fc9eef5c803e5fc
QASAK <w663012911@outlook.com>
2020-07-20 16:11:09 +0800

    Update 接雨水.cpp

--- basic/接雨水.cpp
+++ basic/接雨水.cpp
@@ -8,18 +8,6 @@ public:
     bar* top[1<<14];
     bar ht[1<<14]={0};
     int dp[1<<12];
-    int max(int a, int b) {
-        if(a>b)
-            return a;
-        else 
-            return b;
-    }
-    int max(int a, int b, int c) {
-        if(max(a,b)>c)
-            return max(a,b);
-        else 
-            return c;
-    }
     int sum_ij(bar* top_max, bar* top_next) {
         int s_ij=0;
         bar* start;
@@ -33,7 +21,6 @@ public:
         }
 
         int bound = min(start->val, end->val);
-        // printf("bound: %d\n", bound);
         for(int i=1; i<=(end-start); i++) {
             if(start[i].istop)
                 start[i].istop=false;
@@ -41,9 +28,6 @@ public:
             s_ij+=(bound-start[i].val)>0?bound-start[i].val:0;
             start[i].val=top[0]->val;
         }
-        
-
-        // printf("s_ij %d\n", s_ij);
         return s_ij;
     }
     static bool cmp(bar* a, bar* b) {
@@ -65,23 +49,19 @@ public:
             cur=ht[i].val;
             next=ht[i+1].val;
             if(cur>prev&&cur>=next) {
-                // printf("top: %d\n", cur);
                 ht[i].istop=true;
                 top[p++]=(&ht[i]);
             }
         }
         sort(top, top+p, cmp);
         
-        // for(int i=0; i<p;i++) {
-        //     printf("top[%d]->val: %d\n", i, top[i]->val);
-        // }
         for(int i=1; i<p;i++) {
             if(top[i]->istop) {
-                // printf("top[%d]->val: %d\n", i, top[i]->val);
                 sum+=sum_ij(top[0], top[i]);
             }
         }
-        
-        
         return sum;
     }
+
+
+};
