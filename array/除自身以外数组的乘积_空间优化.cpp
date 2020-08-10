c2e247b55d6cd5bc5f500268098d464fac95c77e
QASAK <w663012911@outlook.com>
2020-08-10 19:32:49 +0800

    update

--- array/除自身以外数组的乘积_空间优化.cpp
+++ array/除自身以外数组的乘积_空间优化.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> ans;
-
         if(nums.empty())
             return ans;
         int n=nums.size();
-        int pre[n];
-        int post[n];
         int prod=1;
-        pre[0]=1;
+        ans.push_back(1);
         for(int i=1;i<n;i++) {
             prod*=nums[i-1];
-            pre[i]=prod;
+            ans.push_back(prod);
         }
         prod=1;
-        post[n-1]=1;
-        for(int i=n-2;i>=0;i--) {
-            prod*=nums[i+1];
-            post[i]=prod;
-        }
-        for(int i=0;i<n;i++) {
-            ans.push_back(pre[i]*post[i]);
+        for(int i=n-1;i>=0;i--) {
+            ans[i]*=prod;
+            prod*=nums[i];
         }
         return ans;
     }
