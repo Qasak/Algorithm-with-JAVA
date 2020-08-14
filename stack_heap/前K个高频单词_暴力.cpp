9db8b2a967c602c1d0760ad99c96a1e1b73127c2
QASAK <w663012911@outlook.com>
2020-08-14 09:13:22 +0800

    Update 前K个高频单词_暴力.cpp

--- stack_heap/前K个高频单词_暴力.cpp
+++ stack_heap/前K个高频单词_暴力.cpp
@@ -1,19 +1,19 @@
 bool cmp (const pair<string, int> &a, const pair<string, int> &b) {
-    return a.second>b.second;
+    if(a.second==b.second)return a.first<b.first;
+    else return a.second>b.second;
 }
+
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         map<string, int> m;
-        vector<pair<string, int> > tmp;
+        
         vector<string> ans;
         for(auto &word: words) {
             m[word]++;
         }
-        for(const auto &i:m) {
-            tmp.push_back(make_pair(i.first, i.second));
-        }
-        stable_sort(tmp.begin(), tmp.end(), cmp);
+        vector<pair<string, int> > tmp(m.begin(), m.end()); // 直接放begin() end()就可以转换
+        sort(tmp.begin(), tmp.end(), cmp);
         for(const auto &item:tmp) {
             if(k--==0) break;
             ans.push_back(item.first);
