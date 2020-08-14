af95e89ed9edb6c03e998be1037d5ee4e3500dee
QASAK <w663012911@outlook.com>
2020-08-14 08:52:06 +0800

    update

--- stack_heap/前K个高频单词.py
+++ stack_heap/前K个高频单词.py
@@ -5,3 +5,10 @@ class Solution:
         most_common = [i[0] for i in sorted(counter.items(), key=lambda pair: (-pair[1], pair[0]))]
         return most_common[:k]
         
+class Solution(object):
+    def topKFrequent(self, words, k):
+        count = collections.Counter(words)
+        candidates = count.keys()
+        candidates.sort(key = lambda w: (-count[w], w))
+        return candidates[:k]
+
