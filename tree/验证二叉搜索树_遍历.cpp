b7b003915daf018746522d75b1b3e722792b7cdd
QASAK <w663012911@outlook.com>
2020-08-25 16:26:40 +0800

    Create 验证二叉搜索树_遍历.cpp

---
+++ tree/验证二叉搜索树_遍历.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+#define MAX_INT ((unsigned)(-1)>>1)
+
+#define MIN_INT (~MAX_INT)
+
+class Solution {
+public:
+    vector<int> vals;
+    void traverse(TreeNode* root) {
+        if(root->left)  traverse(root->left);
+
+        vals.push_back(root->val);
+
+        if(root->right) traverse(root->right);
+    }
+    bool isValidBST(TreeNode* root) {
+        if(!root)
+            return true;
+        traverse(root);
+        for(int i=0;i<vals.size()-1;i++) {
+            if(vals[i]>=vals[i+1])
+                return false;
+        }
+        return true;
+    }
+};
