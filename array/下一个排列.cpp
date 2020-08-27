50e411a61b96752e32ee445ddf5993a9c8f6c41e
QASAK <w663012911@outlook.com>
2020-08-27 09:31:08 +0800

    Update 下一个排列.cpp

--- array/下一个排列.cpp
+++ array/下一个排列.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        if(n<2) 
-            return;
-        int i=n-2;
-        int j=n-1;
-        
-        while(i>=0 && nums[i]>=nums[j]) {i--;j--;}
-        if (i!=-1) {
-            int k=n-1;
-            while(k>j && nums[k]<=nums[i]) k--;
-            swap(nums[i],nums[k]);
+        int pos=nums.size()-1;
+        while(pos>0 && nums[pos]<=nums[pos-1]) pos--;
+		
+        if (pos!=0) {
+            int k=nums.size()-1;
+            while(k>pos && nums[k]<=nums[pos-1]) k--;
+            swap(nums[pos-1],nums[k]);
         }
-
-        while(j<n-1) {
-            swap(nums[j], nums[n-1]);
-            j++;
-            n--;
-        }
+		sort(nums.begin()+pos, nums.end());
         return;
     }
 };
