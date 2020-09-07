2b4bee3a24d8235a12e4f61909fb9ceff32faf80
QASAK <w663012911@outlook.com>
2020-09-07 10:20:24 +0800

    Update 无重复字符的最长子串.cpp

--- string/无重复字符的最长子串.cpp
+++ string/无重复字符的最长子串.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int first=0;
-    int last=0;
-    int mxcnt=0;
     int lengthOfLongestSubstring(string s) {
-        int n=s.size();
-        while(last<n) {
-            bool ascii[127] = {0};
-            bool flag=true;
-            int cnt=0;
-            while(last<n && flag) {
-                int idx=(int)s[last];
-                if(ascii[idx]) {
-                    first++;
-                    flag=false;
-                    last=first;
-                } else {
-                    ascii[idx]=1;
-                    last++;
-                    cnt++;
-                    if(cnt>mxcnt) {
-                        mxcnt=cnt;
-                    }
-                }
+        int n,l,r,cnt,mxcnt;
+        n=s.size();
+        mxcnt=0;
+        unordered_map<char, bool> mp;
+        for(int i=0;i<n;i++) {
+            cnt=0;
+            l=r=i;
+            mp.clear();
+            while(r<n) {
+                if(mp.count(s[r]))
+                    break;
+                mp[s[r++]]=true;
+                if(++cnt>mxcnt)
+                    mxcnt=cnt;
             }
         }
         return mxcnt;
