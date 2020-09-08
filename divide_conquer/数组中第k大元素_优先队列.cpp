d50c3dcc5d087868cab04e9c85f4ccbb77ee97e4
QASAK <w663012911@outlook.com>
2020-09-08 15:37:17 +0800

    update

--- divide_conquer/数组中第k大元素_优先队列.cpp
+++ divide_conquer/数组中第k大元素_优先队列.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
-        for(int &c:nums) {
-            pq.push(c);
+        priority_queue<int, vector<int>> q;
+        int ans;
+        for(int num:nums) {
+            q.push(num);
         }
-        while(k-1) {
-            k--;
-            pq.pop();
+        while(k--) {
+            ans=q.top();
+            q.pop();
         }
-        return pq.top();
+        return ans;
     }
 };
