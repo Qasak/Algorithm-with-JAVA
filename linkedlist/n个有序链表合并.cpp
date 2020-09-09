4de556d19849486433fc270c7e5c6af439d3308d
QASAK <w663012911@outlook.com>
2020-09-09 16:52:40 +0800

    update

--- linkedlist/n个有序链表合并.cpp
+++ linkedlist/n个有序链表合并.cpp
@@ -38,7 +38,61 @@ public:
     }
 };
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* ans,*p;
+        p=new ListNode();
+        ans=p;
 
+        while(l1&&l2) {
+            if(l1->val<l2->val) {
+                p->next=l1;
+                l1=l1->next;
+                p=p->next;
+                
+                
+            } else {
+                p->next=l2;
+                l2=l2->next;
+                p=p->next;
+                
+                
+            }
+        }
+        if(!l1) {
+            p->next=l2;
+        } else {
+            p->next=l1;
+        }
+        
+        return ans->next;
+    }
+    ListNode* p[10005]={nullptr};
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n=lists.size();
+        if(n==0) return nullptr;
+        if(n==1) return lists[0];
+        
+        p[0]=lists[0];
+        for(int i=1;i<n;i++) {
+            
+            p[i]=mergeTwoLists(p[i-1], lists[i]);
+        }
+        
+        return p[n-1];
+    }
+};
 
 #include <iostream>
 using namespace std;
--- tree/最近公共祖先.cpp
+++ tree/最近公共祖先.cpp
@@ -1,36 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-typedef long long ll;
+
 class Solution {
 public:
-    static const int MAX_V=1<<16;
-    TreeNode* parents[MAX_V]={nullptr};
-    ll depth[MAX_V]={0};
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        dfs(root, nullptr, 0);
+        return lca(p,q);
+    }
+private:
+    unordered_map<int, TreeNode*> parents;
+    unordered_map<int, int> depth;
     void dfs(TreeNode* node, TreeNode* p, int d) {
         if(!node) return;
-        parents[node->val+(1<<14)]=p;
-        depth[node->val+(1<<14)]=d;
+        parents[node->val]=p;
+        depth[node->val]=d;
         dfs(node->left, node, d+1);
         dfs(node->right, node, d+1);
     }
     TreeNode* lca(TreeNode* p, TreeNode* q) {
-        while(depth[p->val+(1<<14)]>depth[q->val+(1<<14)])  p=parents[p->val+(1<<14)];
-        while(depth[q->val+(1<<14)]>depth[p->val+(1<<14)])  q=parents[q->val+(1<<14)];
+        while(depth[p->val]>depth[q->val])  p=parents[p->val];
+        while(depth[q->val]>depth[p->val])  q=parents[q->val];
         while(p!=q) {
-            p=parents[p->val+(1<<14)];
-            q=parents[q->val+(1<<14)];
+            p=parents[p->val];
+            q=parents[q->val];
         }
         return p;
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        dfs(root, nullptr, 0);
-        return lca(p,q);
     }
 };
