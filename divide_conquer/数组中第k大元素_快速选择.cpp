c1f24178814313a050fb5a43185d0ab963c38bc3
QASAK <w663012911@outlook.com>
2020-09-14 15:07:09 +0800

    update

--- divide_conquer/数组中第k大元素_快速选择.cpp
+++ divide_conquer/数组中第k大元素_快速选择.cpp
@@ -1,19 +1,19 @@
-class Solution {
+ class Solution {
 public:
 	int quick_select(vector<int>& nums, int l, int r, int idx) {
 		int p=random_partition(nums, l, r);
 		if(p==idx)
 			return nums[p];
-		return p>idx ? quick_select(nums, l, p, idx):quick_select(nums, p+1, r, idx);
+		return p>idx ? quick_select(nums, l, p-1, idx):quick_select(nums, p+1, r, idx);
 	}
 	int random_partition(vector<int>& nums, int l, int r) {
-		int i=rand() % (r-l) + l;
-		swap(nums[i], nums[r-1]);
+		int i=rand() % (r-l+1) + l;
+		swap(nums[i], nums[r]);
 		return partition(nums, l, r);
 	}
 	int partition(vector<int>& nums, int l, int r) {
 		int i=l-1;
-		int x=nums[r-1];
+		int x=nums[r];
 		for(int j=l; j<r-1;j++) {
 			if(nums[j]<=x) {
 				i++;
@@ -25,6 +25,6 @@ public:
 	}
     int findKthLargest(vector<int>& nums, int k) {
 		srand(time(0));
-		return quick_select(nums, 0, nums.size(), nums.size()-k);
+		return quick_select(nums, 0, nums.size()-1, nums.size()-k);
     }
 };
