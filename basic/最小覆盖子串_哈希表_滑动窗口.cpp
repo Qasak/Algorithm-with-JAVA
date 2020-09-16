460bb09bb58a6cea32f46a90f9968a886ef1b4a8
QASAK <w663012911@outlook.com>
2020-09-16 13:01:15 +0800

    update

--- basic/最小覆盖子串_哈希表_滑动窗口.cpp
+++ basic/最小覆盖子串_哈希表_滑动窗口.cpp
@@ -22,50 +22,75 @@
 
 class Solution {
 public:
-    unordered_map<char, int> pattern, tmp;
-    const int MAX_INT=(unsigned(1<<31>>31))>>1;
+    unordered_map<char, int> charset_t, charset_s;
     bool check() {
-        for(const auto &c:pattern) {
-            if(tmp[c.first]<c.second)
+        for(auto c_i:charset_t) {
+            char t=c_i.first;
+            if(charset_s[t]<c_i.second)
                 return false;
         }
         return true;
     }
-    
-    
     string minWindow(string s, string t) {
-        for(const auto &c:t) {
-            ++pattern[c];
+        int n=s.size();
+        int m=t.size();
+        if(!n || !m) return "";
+        for(int i=0; i<m;i++) {
+            charset_t[t[i]]++;
         }
-        int left,right,min_left,min_right;
-        int min_len=MAX_INT;
-        left=0;
-        right=0;
-        for(const auto &c:s) {
-            if(pattern.find(c)!=pattern.end()) {
-                ++tmp[c];
+        int l,r,min_l,min_r;
+        int min_len=INT_MAX;
+        l=r=min_l=min_r=0;
+        while(r<n) {
+            if(charset_t.find(s[r])!=charset_t.end()) { // 如果是T中的字符
+                charset_s[s[r]]++;
             }
-                
-            
-            while(check() && right<s.size()) {
-                if(right-left+1<min_len) {
-                    min_left=left;
-                    min_right=right;
-                    min_len=right-left+1;
+            while(check() && r<s.size()) { // S的窗口中的字符数量和T的字符数量匹配
+                if(r-l+1<min_len) { // 该窗口小于之前的窗口：记录l,r和长度
+                    min_l=l;
+                    min_r=r;
+                    min_len=r-l+1;
                 }
-                if(tmp.find(s[left])!=tmp.end())
-                    tmp[s[left]]--;
-                ++left;
+                if(charset_s.find(s[l])!=charset_s.end()) // 移动窗口左端点，若左端点字符在集合中，则数量--， 直到匹配不上
+                    charset_s[s[l]]--;
+                l++;
             }
-            ++right;
-
+            r++;
         }
-        if(min_len==MAX_INT)
+        if(min_len==INT_MAX)
             return "";
-        char ans[s.size()+1];
-        for(int i=min_left;i<=min_right;i++) {
-            sprintf(ans+i-min_left, "%c", s[i]);
+        return s.substr(min_l, min_r-min_l+1);
+    }
+};
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        int mp[128]={0}; // ASCII一共128个
+        for (int i=0;i<m;i++) // t中的所有元素加入集合
+            mp[t[i]]++;
+        int l, r, min_l, min_r, min_len;
+        l=r=min_l=min_r=0;
+        min_len=INT_MAX;
+        while(r<n) {
+            if(mp[s[r]]-- > 0) { // 超出的元素继续减，但不再减集合的大小
+                m--;
+            }
+            while(m==0) {
+                if(r-l+1<min_len) {
+                    min_l=l;
+                    min_r=r;
+                    min_len=r-l+1;
+                }
+                if(mp[s[l]]++ ==0) { // 恢复集合: "ADOBECODEBANC" 注意B出现两次,mp值-1, 如果if(mp[s[l]] ==0) 则不能恢复到集合中
+                    m++;
+                }
+                l++;
+            }
+            r++;
         }
-        return ans;
+        return min_len==INT_MAX ? "" : s.substr(min_l, min_r-min_l+1);
     }
 };
