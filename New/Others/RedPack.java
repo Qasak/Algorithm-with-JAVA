5c44066fcf06b1225bc38ff2e3cf155c4263f0d0
QASAK <w663012911@outlook.com>
2021-04-30 18:21:09 +0800

    Update RedPack.java

--- New/Others/RedPack.java
+++ New/Others/RedPack.java
@@ -28,26 +28,25 @@ public class RedPack {
     private double remainMoney;
     public RedPack(int size, double money) {
         remainSize = size;
-        remainMoney = money;
+        remainMoney = money - 0.01 * size;
     }
     // 二倍均值法：每次抢的金额 = [0.01, 2 * (money / size))
     public double getRandomMoney() {
         if(remainSize == 1) {
             remainSize--;
-            return (double) Math.round(remainMoney * 100) / 100;
+            // 但是这里需要四舍五入
+            return (double) Math.round(remainMoney * 100) / 100 + 0.01;
         }
         Random rand = new Random();
         double min = 0.01;
         double max = (remainMoney / remainSize) * 2;
         double ret = rand.nextDouble() * max;
-        ret = Math.max(ret, min);
-
+//        ret = Math.max(ret, min);
         // 两种舍入均可
-        //
-        ret = Math.floor(ret * 100) / 100;
-//        ret = (double) Math.round(ret * 100) / 100;
+//        ret = Math.floor(ret * 100) / 100;
+        ret = (double) Math.round(ret * 100) / 100;
         remainSize--;
         remainMoney -= ret;
-        return ret;
+        return ret + 0.01;
     }
 }
