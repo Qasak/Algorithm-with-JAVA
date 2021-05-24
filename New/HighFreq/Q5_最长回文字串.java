b97efdf38786938997e3e7d0355f8e8034d1df81
QASAK <w663012911@outlook.com>
2021-05-24 18:17:26 +0800

    Update Q5_最长回文字串.java

--- New/HighFreq/Q5_最长回文字串.java
+++ New/HighFreq/Q5_最长回文字串.java
@@ -39,7 +39,36 @@ public class Q5_最长回文字串 {
         }
         return s.substring(x, y + 1);
     }
+    public String longestPalindrome1(String s) {
+        int n = s.length();
+        boolean[][] f = new boolean[n][n];
+        String ans = "";
+        int max = 0;
+        for(int len = 1; len <= n; len++) {
+            for(int l = 0; l + len - 1 < n; l++) {
+                int r = l + len - 1;
+                if(len == 1) {
+                    f[l][r] = true;
+                } else if(len == 2) {
+                    if(s.charAt(l) == s.charAt(r)) {
+                        f[l][r] = true;
+                    } 
+                } else {
+                    if(s.charAt(l) == s.charAt(r)) {
+                        f[l][r] = f[l + 1][r - 1];
+                    }
+                }
+                if(f[l][r]) {
+                    if(len > max) {
+                        ans = s.substring(l, r + 1);
+                        max = len;
+                    }
+                }
+            }
 
+        }
+        return ans;
+    }
     // 中心扩散
 
     // Manacher 算法 O(n)
