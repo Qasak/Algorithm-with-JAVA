928890c29119a9c1b3ec43099413b6809ddbb37e
QASAK <w663012911@outlook.com>
2021-06-24 16:18:29 +0800

    Update Q149_直线上最多的点.java

--- New/NiceProblem/Q149_直线上最多的点.java
+++ New/NiceProblem/Q149_直线上最多的点.java
@@ -1,5 +1,8 @@
 package leetcode.contest.NiceProblem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Wangjs
  * @version 1.0
@@ -36,5 +39,43 @@ public class Q149_直线上最多的点 {
         }
         return ans;
     }
-    // 2.
+    // 2. 枚举点对
+    // 哈希表保存 [斜率:点数]对，当心+0/-0 +∞/-∞
+    public int maxPoints1(int[][] points) {
+        int ans = 1;
+        int n = points.length;
+        // y = kx + b
+        // y1 = kx1 + b;
+        // y2 = kx2 + b;
+        // y3 = kx3 + b;
+        // y1 - y2 = k(x1 - x2)
+        // k = (y1 - y2) / (x1 - x2)
+        // k' = (y1 - y3) / (x1 - x3)
+        // (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2)
+
+        for(int i = 0; i < n; i++) {
+            int[] a = points[i];
+            Map<Double, Integer> map = new HashMap<>();
+            for(int j = i + 1; j < n; j++) {
+                int[] b = points[j];
+                double dy = (a[1] - b[1]) * 1.0;
+                double dx = (a[0] - b[0]) * 1.0;
+                double k;
+                if(dy == 0) {
+                    k = 0;
+                } else if(dx == 0) {
+                    k = Double.MAX_VALUE;
+                } else {
+                    k = dy / dx;
+                }
+                if(map.containsKey(k)) {
+                    map.put(k, map.get(k) + 1);
+                } else {
+                    map.put(k, 2);
+                }
+                ans = Math.max(ans, map.get(k));
+            }
+        }
+        return ans;
+    }
 }
