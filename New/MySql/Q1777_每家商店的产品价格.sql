5b09dec98d6203d1b7b9393eeda6ddee0bc477b8
QASAK <w663012911@outlook.com>
2021-07-07 23:44:17 +0800

    update

--- New/MySql/Q1777_每家商店的产品价格.sql
+++ New/MySql/Q1777_每家商店的产品价格.sql
@@ -1,3 +1,5 @@
+# 在mysql中的语句执行顺序是
+# from -> on -> join -> where -> group by -> 聚集函数 -> having -> select ->distinct -> union -> order by -> limit
 select product_id,
        sum(if(store='store1',price,null)) store1,
        sum(if(store='store2',price,null)) store2,
