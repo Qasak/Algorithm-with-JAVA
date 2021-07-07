8a999af5cff04bdcbcc374fea84f48dff976018b
QASAK <w663012911@outlook.com>
2021-07-07 09:38:17 +0800

    Create Q1921_消灭怪物的最大数量.java

---
+++ New/NiceProblem/WeekContest_248/Q1921_消灭怪物的最大数量.java
@@ -0,0 +1,30 @@
+package leetcode.contest.WeekContest_248;
+
+import java.util.Arrays;
+
+/**
+ * @author Wangjs
+ * @version 1.0
+ * @date 2021/7/7 9:09
+ */
+public class Q1921_消灭怪物的最大数量 {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        int n = dist.length;
+        int[] tmp = new int[n];
+        for(int i = 0; i < n; i++) {
+            tmp[i] = dist[i] / speed[i];
+            if(dist[i] > speed[i] && dist[i] % speed[i] != 0) {
+                tmp[i]++;
+            }
+        }
+        int ans = 1;
+        Arrays.sort(tmp);
+        for(int i = 1; i < n; i++) {
+            if(tmp[i] <= i) {
+                break;
+            }
+            ans++;
+        }
+        return ans;
+    }
+}
