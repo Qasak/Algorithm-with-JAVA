7f1c5758aebfb8001661ebd4c3a60b116c5d77b1
QASAK <w663012911@outlook.com>
2021-07-07 19:22:14 +0800

    update

---
+++ New/NiceProblem/字符串哈希技巧/Q718_最长重复子数组.java
@@ -0,0 +1,70 @@
+package leetcode.contest.NiceProblem.字符串哈希技巧;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Wangjs
+ * @version 1.0
+ * @date 2021/7/7 18:46
+ */
+public class Q718_最长重复子数组 {
+    // 滚动哈希 (Rabin-carp)
+    // 类似于java中的String.hashCode()
+    int mod = 1000000009;
+    int base = 100003;
+
+    public int findLength(int[] A, int[] B) {
+        int left = 1, right = Math.min(A.length, B.length) + 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (check(A, B, mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left - 1;
+    }
+
+    public boolean check(int[] A, int[] B, int len) {
+        long hashA = 0;
+        for (int i = 0; i < len; i++) {
+            hashA = (hashA * base + A[i]) % mod;
+        }
+        Set<Long> bucketA = new HashSet<Long>();
+        bucketA.add(hashA);
+        long mult = qPow(base, len - 1);
+        for (int i = len; i < A.length; i++) {
+            hashA = ((hashA - A[i - len] * mult % mod + mod) % mod * base + A[i]) % mod;
+            bucketA.add(hashA);
+        }
+        long hashB = 0;
+        for (int i = 0; i < len; i++) {
+            hashB = (hashB * base + B[i]) % mod;
+        }
+        if (bucketA.contains(hashB)) {
+            return true;
+        }
+        for (int i = len; i < B.length; i++) {
+            hashB = ((hashB - B[i - len] * mult % mod + mod) % mod * base + B[i]) % mod;
+            if (bucketA.contains(hashB)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 使用快速幂计算 x^n % mod 的值
+    public long qPow(long x, long n) {
+        long ret = 1;
+        while (n != 0) {
+            if ((n & 1) != 0) {
+                ret = ret * x % mod;
+            }
+            x = x * x % mod;
+            n >>= 1;
+        }
+        return ret;
+    }
+}
---
+++ New/NiceProblem/滑动窗口技巧/Q718_最长重复子数组.java
@@ -0,0 +1,9 @@
+package leetcode.contest.NiceProblem.滑动窗口技巧;
+
+/**
+ * @author Wangjs
+ * @version 1.0
+ * @date 2021/7/7 18:47
+ */
+public class Q718_最长重复子数组 {
+}
