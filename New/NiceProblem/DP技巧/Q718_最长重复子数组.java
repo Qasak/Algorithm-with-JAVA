818d4d5aee8d43f3bf752eb29e3f3d25ea0dbc62
QASAK <w663012911@outlook.com>
2021-07-07 18:45:39 +0800

    Update Q718_最长重复子数组.java

--- New/NiceProblem/DP技巧/Q718_最长重复子数组.java
+++ New/NiceProblem/DP技巧/Q718_最长重复子数组.java
@@ -6,4 +6,20 @@ package leetcode.contest.NiceProblem.DP技巧;
  * @date 2021/7/7 18:37
  */
 public class Q718_最长重复子数组 {
+    public int findLength(int[] A, int[] B) {
+        int n = A.length, m = B.length;
+        // A中前i个元素与B中前j个元素的最长公共子数组
+        // 不是子序列
+        int ans = 0;
+        int[][] f = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(A[i - 1] == B[j - 1]) {
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                }
+                ans = Math.max(ans, f[i][j]);
+            }
+        }
+        return ans;
+    }
 }
