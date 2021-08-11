22a1e32af451f69fe36d692b333a30bc01d4f88c
QASAK <w663012911@outlook.com>
2021-08-11 23:56:22 +0800

    Update Q1035_不相交的线.java

--- New/Mitsuha/序列DP/Q1035_不相交的线.java
+++ New/Mitsuha/序列DP/Q1035_不相交的线.java
@@ -6,4 +6,20 @@ package Mitsuha.序列DP;
  * @date 2021/8/11 18:04
  */
 public class Q1035_不相交的线 {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        // f[i][j] : nums1[0, i] 和 nums2[0, j] 子序列的最大连线数
+        int[][] f = new int[n + 1][m + 1];
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 1; j < m + 1; j++) {
+                if(nums1[i - 1] == nums2[j - 1]) {
+                    f[i][j] = Math.max(f[i][j], f[i - 1][j - 1]) + 1;
+                } else {
+                    f[i][j] = Math.max(f[i][j], Math.max(f[i - 1][j], f[i][j - 1]));
+                }
+            }
+        }
+        return f[n][m];
+    }
 }
