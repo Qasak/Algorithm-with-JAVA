c48b932a6131f6ae7f2034b586e9112fd976d334
QASAK <w663012911@outlook.com>
2021-08-12 15:25:30 +0800

    Update Q516_最长回文子序列.java

--- New/Mitsuha/区间DP/Q516_最长回文子序列.java
+++ New/Mitsuha/区间DP/Q516_最长回文子序列.java
@@ -23,4 +23,28 @@ public class Q516_最长回文子序列 {
         }
         return f[n][n];
     }
+
+    // 通常做法
+    public int longestPalindromeSubseq1(String s) {
+        char[] cs = s.toCharArray();
+        int n = s.length();
+        int[][] f = new int[n][n];
+        for(int len = 0; len < n; len++) {
+            for(int i = 0; i + len < n; i++) {
+                int j = i + len;
+                // [i, j]
+                if(len == 0) {
+                    f[i][j] = 1 ;
+                } else if(len == 1) {
+                    f[i][j] = cs[i] == cs[j] ? 2 : 1;
+                } else {
+                    f[i][j] = Math.max(f[i + 1][j], f[i][j - 1]);
+                    if(cs[i] == cs[j]) {
+                        f[i][j] = Math.max(f[i][j], f[i + 1][j - 1] + 2);
+                    }
+                }
+            }
+        }
+        return f[0][n - 1];
+    }
 }
