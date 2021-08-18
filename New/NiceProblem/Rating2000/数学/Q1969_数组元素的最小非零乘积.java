0736264c2e3f92b1dd47c8c4fc364f0cf309e870
QASAK <w663012911@outlook.com>
2021-08-18 21:38:57 +0800

    Create Q1969_数组元素的最小非零乘积.java

---
+++ New/NiceProblem/Rating2000/数学/Q1969_数组元素的最小非零乘积.java
@@ -0,0 +1,11 @@
+package leetcode.contest.Rating2000.数学;
+
+/**
+ * @author Wangjs
+ * @version 1.0
+ * @date 2021/8/18 21:01
+ */
+public class Q1969_数组元素的最小非零乘积 {
+    // py3
+    //         return ((1 << p) - 1) * pow(((1 << p) - 2), ((1 << (p - 1)) - 1), 1000000007) % 1000000007
+}
