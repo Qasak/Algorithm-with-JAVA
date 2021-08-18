62f74369bbc4bc731b5e8dc555ec443955de9a39
QASAK <w663012911@outlook.com>
2021-08-18 17:08:10 +0800

    Update Q552_学生出勤记录II.java

--- New/Mitsuha/状态机DP/Q552_学生出勤记录II.java
+++ New/Mitsuha/状态机DP/Q552_学生出勤记录II.java
@@ -10,6 +10,7 @@ public class Q552_学生出勤记录II {
     int mod = (int) 1e9 + 7;
     public int checkRecord(int n) {
         // f[i][A][L]
+        // 下标i结尾，A 的数量，结尾连续 L 的数量
         int[][][] f = new int[n][2][3];
         f[0][1][0] = 1;
         f[0][0][1] = 1;
@@ -47,4 +48,41 @@ public class Q552_学生出勤记录II {
         }
         return ans;
     }
+    public int checkRecord1(int n) {
+        // f[i][A][L]
+        int[][][] f = new int[n][2][3];
+        f[0][1][0] = 1;
+        f[0][0][1] = 1;
+        f[0][0][0] = 1;
+        for(int i = 1; i < n; i++) {
+            // A L P
+            // A
+            for(int k = 0; k < 3; k++) {
+                f[i][1][0] = (f[i][1][0] + f[i - 1][0][k]) % mod;
+            }
+
+            // L
+            for(int j = 0; j < 2; j++) {
+                for(int k = 1; k < 3; k++) {
+                    f[i][j][k] = (f[i][j][k] + f[i - 1][j][k - 1]) % mod;
+                }
+            }
+            // P
+            for(int j = 0; j < 2; j++) {
+                for(int k = 0; k < 3; k++) {
+                    f[i][j][0] = (f[i][j][0] + f[i - 1][j][k]) % mod;
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 3; j++) {
+                ans += f[n - 1][i][j];
+                ans %= mod;
+            }
+        }
+        return ans;
+    }
+    // 矩阵快速幂
+
 }
