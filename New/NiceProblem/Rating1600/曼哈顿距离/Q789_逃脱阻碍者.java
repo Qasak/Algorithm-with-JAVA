0be33c0ef27ecc563e7e895b7592588e26ef96a1
QASAK <w663012911@outlook.com>
2021-08-22 20:27:27 +0800

    Create Q789_逃脱阻碍者.java

---
+++ New/NiceProblem/Rating1600/曼哈顿距离/Q789_逃脱阻碍者.java
@@ -0,0 +1,19 @@
+package leetcode.contest.Rating1600.曼哈顿距离;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Wangjs
+ * @version 1.0
+ * @date 2021/8/22 20:22
+ */
+public class Q789_逃脱阻碍者 {
+    public static void main(String[] args) {
+        int[] a = new int[]{1,2};
+        int[] b = new int[]{1,2};
+        Set<int[]> set = new HashSet<>();
+        set.add(a); set.add(b);
+        System.out.println(set);
+    }
+}
