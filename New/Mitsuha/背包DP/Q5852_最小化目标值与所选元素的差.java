fc38b09ba8c5dfca07a66dd383c671f18cf2d70d
QASAK <w663012911@outlook.com>
2021-08-23 19:11:16 +0800

    Update Q5852_最小化目标值与所选元素的差.java

--- New/Mitsuha/背包DP/Q5852_最小化目标值与所选元素的差.java
+++ New/Mitsuha/背包DP/Q5852_最小化目标值与所选元素的差.java
@@ -6,5 +6,35 @@ package Mitsuha.背包DP;
  * @date 2021/8/23 18:45
  */
 public class Q5852_最小化目标值与所选元素的差 {
+    // 记忆化
+    int diff = 0x3f3f3f3f;
+    int n;
+    int m;
+    // f[i][j]前i行是否存在和洽为j的情况
+    boolean[][] f;
+    public int minimizeTheDifference(int[][] mat, int target) {
+        // 记忆化
+        n = mat.length;
+        m = mat[0].length;
+        f = new boolean[n][4901];
+        dfs(0, target, 0, mat);
+        return diff;
+    }
+    void dfs(int i, int target, int cur, int[][] mat) {
+        if(i == n) {
+            diff = Math.min(diff, Math.abs(cur - target));
+            return;
+        }
+        // 剪枝
+        if(f[i][cur] || cur - target > diff) {
+            return;
+        }
+
+        f[i][cur] = true;
+        for(int j = 0; j < m; j++) {
+            dfs(i + 1, target, cur + mat[i][j], mat);
+        }
+    }
+    // 分组背包
 
 }
