532bcacb0acdc07de3f82bd5ea3f2d803c602aaf
QASAK <w663012911@outlook.com>
2021-09-02 17:56:28 +0800

    Update Q337_打家劫舍III.java

--- New/Mitsuha/树型DP/Q337_打家劫舍III.java
+++ New/Mitsuha/树型DP/Q337_打家劫舍III.java
@@ -20,3 +20,24 @@ class Solution {
         g.put(root, Math.max(g.getOrDefault(root.left, 0), f.getOrDefault(root.left, 0)) + Math.max(g.getOrDefault(root.right, 0), f.getOrDefault(root.right, 0)));
     }
 }
+
+// 空间优化
+class Solution {
+    public int rob(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int[] t = dfs(root);
+        return Math.max(t[0], t[1]);
+    }
+    int[] dfs(TreeNode root) {
+        if(root == null) {
+            return new int[]{0, 0};
+        }
+        int[] l = dfs(root.left);
+        int[] r = dfs(root.right);
+        int a = root.val + l[1] + r[1];
+        int b = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return new int[]{a, b};
+    }
+}
